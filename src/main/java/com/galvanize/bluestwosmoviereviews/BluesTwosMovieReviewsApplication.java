39038c6ba461f52749d2eaa4fb239d13e9acd938
Justin Jewell <justinjewell@gmail.com>
2023-03-27 11:19:45 -0500

    initial project files

---
+++ src/main/java/com/galvanize/bluestwosmoviereviews/BluesTwosMovieReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.bluestwosmoviereviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluesTwosMovieReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluesTwosMovieReviewsApplication.class, args);
+    }
+
+}
