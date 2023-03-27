8e028cb6401063629b0d507f3548d185e203d194
patricklapatra <patrick.lapatra@usaa.com>
2023-03-27 11:49:39 -0500

    Initial branch test commit

--- src/main/java/com/galvanize/bluestwosmoviereviews/BluesTwosMovieReviewsApplication.java
+++ src/main/java/com/galvanize/bluestwosmoviereviews/BluesTwosMovieReviewsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BluesTwosMovieReviewsApplication {
 
     public static void main(String[] args) {
-        // testing git rebase
         SpringApplication.run(BluesTwosMovieReviewsApplication.class, args);
     }
 
