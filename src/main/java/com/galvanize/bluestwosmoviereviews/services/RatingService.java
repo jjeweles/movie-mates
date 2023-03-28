3d1d24b8687a2b9980dd7564de3823f6716656d2
stephen <stephenfhopkins@gmail.com>
2023-03-28 09:29:35 -0700

    Created RatingService

--- src/main/java/com/galvanize/bluestwosmoviereviews/services/RatingService.java
+++ src/main/java/com/galvanize/bluestwosmoviereviews/services/RatingService.java
@@ -4,6 +4,8 @@ import com.galvanize.bluestwosmoviereviews.data.RatingRepository;
 import com.galvanize.bluestwosmoviereviews.models.RatingModel;
 import com.galvanize.bluestwosmoviereviews.models.UserModel;
 
+import java.util.List;
+
 public class RatingService {
 
     RatingRepository ratingRepository;
@@ -12,13 +14,14 @@ public class RatingService {
         this.ratingRepository = ratingRepository;
     }
 
-    public RatingModel getRating(Integer tdmb_id) {
-        return ratingRepository.findByTdmb()
-    }
-
-
-    public UserModel getUserByID(Integer id) {
-        return userRepository.findById(id).orElse(null);
-    }
+//    public RatingModel getRating(Integer tdmb_id) {
+//        List<RatingModel> ratings;
+//        return ratings;
+//    }
+//
+//
+//    public UserModel getUserByID(Integer id) {
+//        return userRepository.findById(id).orElse(null);
+//    }
 
 }
