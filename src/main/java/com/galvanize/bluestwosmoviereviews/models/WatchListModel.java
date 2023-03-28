be8a42f8a687c796461c4b9b1503039bf1373005
daytien <daytienw@gmail.com>
2023-03-28 10:17:55 -0500

    Created WatchList Model

--- src/main/java/com/galvanize/bluestwosmoviereviews/models/WatchListModel.java
+++ src/main/java/com/galvanize/bluestwosmoviereviews/models/WatchListModel.java
@@ -2,6 +2,38 @@ package com.galvanize.bluestwosmoviereviews.models;
 
 public class WatchListModel {
 
-    String hi = "hi";
+    Integer list_id;
+    Integer imdb_id;
+    Integer user_id;
+
+    public Integer getList_id() {
+        return list_id;
+    }
+
+    public void setList_id(Integer list_id) {
+        this.list_id = list_id;
+    }
+
+    public Integer getImdb_id() {
+        return imdb_id;
+    }
+
+    public void setImdb_id(Integer imdb_id) {
+        this.imdb_id = imdb_id;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public WatchListModel(Integer list_id, Integer imdb_id, Integer user_id){
+        this.list_id = list_id;
+        this.imdb_id = imdb_id;
+        this.user_id = user_id;
+    }
 
 }
