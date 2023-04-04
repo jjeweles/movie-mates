9231bddbeebeed7e19a4633ec98ca9bfadf24fcb
patricklapatra <patrick.lapatra@usaa.com>
2023-04-04 15:25:08 -0500

    ReplyModel, Service and Repo functional.

--- src/main/java/com/galvanize/bluestwosmoviereviews/services/ReplyService.java
+++ src/main/java/com/galvanize/bluestwosmoviereviews/services/ReplyService.java
@@ -1,4 +1,47 @@
 package com.galvanize.bluestwosmoviereviews.services;
 
+import com.galvanize.bluestwosmoviereviews.data.PostRepository;
+import com.galvanize.bluestwosmoviereviews.data.ReplyRepository;
+import com.galvanize.bluestwosmoviereviews.models.PostModel;
+import com.galvanize.bluestwosmoviereviews.models.ReplyModel;
+
 public class ReplyService {
+
+
+    ReplyRepository replyRepository;
+
+    public ReplyService(ReplyRepository replyRepository){
+        this.replyRepository = replyRepository;
+    }
+
+    public Iterable<ReplyModel> getAllReplies() {
+        return replyRepository.findAll();
+    }
+
+    public ReplyModel getReplyByID(Integer userID){
+        return replyRepository.findReplyByUserID(userID);
+    }
+
+    public ReplyModel addPost(ReplyModel reply){
+        return replyRepository.save(reply);
+    }
+
+    public ReplyModel updateReply(Integer replyID, ReplyModel reply){
+        ReplyModel replyToUpdate = replyRepository.findReplyByReplyID(replyID);
+
+        if(replyToUpdate != null){
+            replyToUpdate.setReply_text(reply.getReply_text());
+        }
+
+        assert replyToUpdate != null;
+        return replyRepository.save(replyToUpdate);
+    }
+
+    public ReplyModel deletePost (Integer replyID){
+        ReplyModel replyToDelete = replyRepository.findReplyByReplyID(replyID);
+        if (replyToDelete != null){
+            replyRepository.delete(replyToDelete);
+        }
+        return replyToDelete;
+    }
 }
