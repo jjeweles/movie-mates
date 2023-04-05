79037e3a8509544f5d9b027344dd9a18ae3ddfb2
daytien <daytienw@gmail.com>
2023-04-05 14:25:45 -0500

    Created Tests for WatchList classes

--- src/test/java/com/galvanize/bluestwosmoviereviews/controllers/WatchListControllerTest.java
+++ src/test/java/com/galvanize/bluestwosmoviereviews/controllers/WatchListControllerTest.java
@@ -1,7 +1,90 @@
 package com.galvanize.bluestwosmoviereviews.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.bluestwosmoviereviews.models.WatchListModel;
+import com.galvanize.bluestwosmoviereviews.services.WatchListService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class WatchListControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private WatchListService watchListService;
+
+    private List<WatchListModel> watchList;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        WatchListModel movie1 = new WatchListModel(1, 12345);
+        WatchListModel movie2 = new WatchListModel(1, 67890);
+
+        watchList = Arrays.asList(movie1, movie2);
+    }
+
+    @Test
+    void getWatchListById() throws Exception {
+        when(watchListService.getWatchList(1)).thenReturn(watchList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/watchlist/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(watchList)));
+    }
+
+    @Test
+    void addMovie() throws Exception {
+        WatchListModel movie = new WatchListModel();
+        movie.setUserID(1);
+        movie.setTmdbID(12345);
+
+        when(watchListService.addToWatchList(movie)).thenReturn(movie);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/watchlist/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(movie)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(movie)));
+    }
+
+
+    @Test
+    void deleteMovie() throws Exception {
+        WatchListModel deletedMovie = new WatchListModel(1, 12345);
+
+        when(watchListService.deleteFromWatchList(1, 12345)).thenReturn(deletedMovie);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/watchlist/1/12345"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(deletedMovie)));
+    }
+
+    @Test
+    void deleteAllWatchList() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/watchlist/deleteAll/1"))
+                .andExpect(MockMvcResultMatchers.status().isAccepted());
+    }
 }
--- src/test/java/com/galvanize/bluestwosmoviereviews/services/WatchListServiceTest.java
+++ src/test/java/com/galvanize/bluestwosmoviereviews/services/WatchListServiceTest.java
@@ -1,17 +1,76 @@
 package com.galvanize.bluestwosmoviereviews.services;
 
-import com.galvanize.bluestwosmoviereviews.data.UserRepository;
 import com.galvanize.bluestwosmoviereviews.data.WatchListRepository;
-import com.galvanize.bluestwosmoviereviews.models.UserModel;
 import com.galvanize.bluestwosmoviereviews.models.WatchListModel;
+import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.ArgumentMatchers.any;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
-class WatchListServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
+public class WatchListServiceTest {
 
+    @Mock
+    WatchListRepository watchListRepository;
 
+    WatchListService watchListService;
+
+    @BeforeEach
+    void setup() {
+        watchListService = new WatchListService(watchListRepository);
+    }
+
+    @Test
+    public void testGetWatchlist(){
+        List<WatchListModel> watchList = new ArrayList<>();
+
+        watchList.add(new WatchListModel(1,2));
+        watchList.add(new WatchListModel(1,3));
+        watchList.add(new WatchListModel(1,4));
+
+        when(watchListRepository.findByUserID(1)).thenReturn(watchList);
+
+        List<WatchListModel> result = watchListService.getWatchList(1);
+
+        assertEquals(3, result.size());
+        assertEquals(watchList, result);
+
+    }
+
+    @Test
+    public void testAddToWatchList(){
+        WatchListModel watchListModel = new WatchListModel(1, 2);
+
+        when(watchListRepository.save(any(WatchListModel.class))).thenReturn(watchListModel);
+
+        WatchListModel result = watchListService.addToWatchList(watchListModel);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getTmdbID()).isEqualTo(2);
+
+    }
+
+    @Test
+    public void testDeleteFromWatchList(){
+        WatchListModel watchList = new WatchListModel(1,2);
+
+        when(watchListRepository.findByUserIDAndTmdbID(anyInt(), anyInt())).thenReturn(watchList);
+
+        WatchListModel watchList1 = watchListService.deleteFromWatchList(1,2);
+        assertThat(watchList1).isNotNull();
+        assertThat(watchList1.getTmdbID()).isEqualTo(2);
+    }
 
 
 }
