243d0f44f0ea5d6b0eebcdb9ea64e7015a44e3cb
Justin Jewell <justinjewell@gmail.com>
2023-04-05 15:41:03 -0500

    one more fix

--- movie-app-frontend/src/components/MovCard.tsx
+++ movie-app-frontend/src/components/MovCard.tsx
@@ -36,7 +36,7 @@ function MovCard(props: any) {
                                         <path
                                             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                                     </svg>
-                                    <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">{rating.starRating * 2}</p>
+                                    <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">{rating.starRating * 2 - .23}</p>
                                     <span
                                         className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
                                     <button
@@ -51,9 +51,7 @@ function MovCard(props: any) {
                             <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                                 <div className="fixed inset-0 bg-gray-500 bg-opacity-5 transition-opacity"
                                      aria-hidden="true"></div>
-                                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                                                                &#8203;
-                                                            </span>
+                                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
                                 <div className="inline-block align-bottom bg-stone-800 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
                                     <h3 className="text-lg leading-6 font-medium text-gray-400">
                                         Rate 1 - 5 stars
