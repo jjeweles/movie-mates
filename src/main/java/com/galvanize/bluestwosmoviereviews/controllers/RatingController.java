43aeb3498a4e042fc081fa79ccf5bfbd3bc4da42
stephen <stephenfhopkins@gmail.com>
2023-04-10 09:35:38 -0700

    Ready to merge
    Added comments for Java Docs

--- src/main/java/com/galvanize/bluestwosmoviereviews/controllers/RatingController.java
+++ src/main/java/com/galvanize/bluestwosmoviereviews/controllers/RatingController.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * REST controller for managing rating operations.
+ * <p>
+ * The {@code RatingController} class handles HTTP requests for operations such as
+ * creating, updating, retrieving, and deleting ratings.
+ */
+
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
@@ -15,9 +22,20 @@ public class RatingController {
 
     private final RatingService ratingService;
 
+    /** Constructs a new {@code RatingController} with the specified {@code UserService}.
+     *
+     * @param ratingService the user service used to perform rating-related operations
+     */
+
     public RatingController(RatingService ratingService) {
         this.ratingService = ratingService;
     }
+
+    /**
+     * Retrieves all ratings.
+     *
+     * @return an {@code List} containing all {@code RatingModel} instances
+     */
 
     @GetMapping("rating/get")
     public ResponseEntity<List<RatingModel>> getAllRatings() {
@@ -26,11 +44,27 @@ public class RatingController {
         return ratings == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(ratings);
     }
 
+    /**
+     * Retrieves a rating by their userID.
+     *
+     * @param userId the ID of the user to retrieve ratings by
+     * @return a {@code List} containing the requested {@code RatingModel} instances if found,
+     *         or an empty {@code ResponseEntity} with a no-content status if not found
+     */
+
     @GetMapping("rating/getUserRatings/{userId}")
     public ResponseEntity<List<RatingModel>> getAllRatingsByUserId(@PathVariable Integer userId) {
         List<RatingModel> ratings = ratingService.getAllRatingsByUserId(userId);
         return ratings == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(ratings);
     }
+
+    /**
+     * Retrieves a rating by their userID.
+     *
+     * @param tmdbId the ID of the movie to retrieve ratings by
+     * @return a {@code List} containing the requested {@code RatingModel} instances if found,
+     *         or an empty {@code ResponseEntity} with a no-content status if not found
+     */
 
     @GetMapping("rating/getRatingsForMovie/{tmdbId}")
     public ResponseEntity<List<RatingModel>> getMovieRatingByIdmbId(@PathVariable Integer tmdbId) {
@@ -38,11 +72,27 @@ public class RatingController {
         return listOfRatingsForMovie == null ? ResponseEntity.noContent().build() : ResponseEntity.ok(listOfRatingsForMovie);
     }
 
+    /**
+     * Retrieves a rating by their userID.
+     *
+     * @param tmdbId the ID of the movie to retrieve ratings by
+     * @return a {@code ResponseEntity} containing the average rating of the movies {@code Double} if found,
+     *      *         or an empty {@code ResponseEntity} with a no-content status if not found
+     */
+
     @GetMapping("rating/getMovieAvgMovieRating/{tmdbId}")
     public ResponseEntity<Double> getAvgMovieRating(@PathVariable Integer tmdbId) {
         Double averageRating = ratingService.getAverageMovieRating(tmdbId);
         return averageRating == 0 ? ResponseEntity.noContent().build() : ResponseEntity.ok(averageRating);
     }
+
+    /**
+     * Updates an existing user.
+     *
+     * @param ratingId the ID of the rating to update
+     * @param ratingModel the {@code RatingModel} instance containing the updated data
+     * @return the updated {@code RatingModel} instance
+     */
 
     @PutMapping("rating/update/{ratingId}")
     public ResponseEntity<RatingModel> updateRating(@PathVariable Integer ratingId, @RequestBody RatingModel ratingModel) {
@@ -50,11 +100,26 @@ public class RatingController {
         return new ResponseEntity<>(updateRating, HttpStatus.OK);
     }
 
+    /**
+     * Creates a new rating.
+     *
+     * @param ratingModel the {@code RatingModel} instance to create
+     * @return the created {@code RatingModel} instance
+     */
+
     @PostMapping("rating/save")
     public ResponseEntity<RatingModel> addNewRating(@RequestBody RatingModel ratingModel) {
         RatingModel newRating = ratingService.addNewRating(ratingModel);
         return new ResponseEntity<>(newRating, HttpStatus.CREATED);
     }
+
+    /**
+     * Deletes a user by their ID.
+     *
+     * @param ratingId the ID of the rating to delete
+     * @return a {@code ResponseEntity} containing the deleted {@code RatingModel} instance if found,
+     *         or an empty {@code ResponseEntity} with a no-content status if not found
+     */
 
     @DeleteMapping("rating/delete/{ratingId}")
     public ResponseEntity<RatingModel> deleteRating(@PathVariable Integer ratingId) {
--- src/main/java/com/galvanize/bluestwosmoviereviews/models/RatingModel.java
+++ src/main/java/com/galvanize/bluestwosmoviereviews/models/RatingModel.java
@@ -3,6 +3,11 @@ package com.galvanize.bluestwosmoviereviews.models;
 import javax.persistence.*;
 import java.util.Objects;
 
+/**
+ * Represents a rating in the system with basic information such as ratingID, tmdbId, starRating, and userID.
+ * <p>
+ * This class is annotated with JPA annotations to map it to a database table.
+ */
 @Entity
 @Table(name = "ratings")
 public class RatingModel {
@@ -21,6 +26,20 @@ public class RatingModel {
     @Column(name = "userID")
     private int userID;
 
+    /**
+     * Default constructor for the RatingModel class, required by JPA.
+     */
+    public RatingModel() {
+    }
+
+    /**
+     * Constructs a new UserModel with the provided username, password, email, and name.
+     *
+     * @param ratingId the id of the rating
+     * @param tmdbId the id of the movie for the rating
+     * @param starRating    the rating score given for the rating
+     * @param userID     the id of the user giving the rating
+     */
     public RatingModel(Integer ratingId, Integer tmdbId, Integer starRating, int userID) {
         this.ratingId = ratingId;
         this.tmdbId = tmdbId;
@@ -28,37 +47,74 @@ public class RatingModel {
         this.userID = userID;
     }
 
-    public RatingModel() {
-    }
-
+    /**
+     * Returns the rating ID of the rating.
+     *
+     * @return the rating ID as an Integer
+     */
     public Integer getRatingId() {
         return ratingId;
     }
 
+    /**
+     * Sets the rating ID of the rating.
+     *
+     * @param ratingId for the new ratingId of the rating
+     */
     public void setRatingId(Integer ratingId) {
         this.ratingId = ratingId;
     }
 
+    /**
+     * Returns the tmdb ID of the rating.
+     *
+     * @return the user ID as an int
+     */
     public int getTmdbId() {
         return tmdbId;
     }
 
+    /**
+     * Sets the tmdb ID of the rating.
+     *
+     * @param tmdbId for the new tmbdId of the rating
+     */
     public void setTmdbId(int tmdbId) {
         this.tmdbId = tmdbId;
     }
 
+    /**
+     * Returns the user starRating of the rating.
+     *
+     * @return the user ID as an int
+     */
     public int getStarRating() {
         return starRating;
     }
 
+    /**
+     * Sets the starRating ID of the rating.
+     *
+     * @param starRating for the new starRating of the rating
+     */
     public void setStarRating(int starRating) {
         this.starRating = starRating;
     }
 
+    /**
+     * Returns the user ID of the rating.
+     *
+     * @return the user ID as an int
+     */
     public int getUserID() {
         return userID;
     }
 
+    /**
+     * Sets the user ID of the rating.
+     *
+     * @param userId for the new userId of the rating
+     */
     public void setUserID(int userId) {
         this.userID = userId;
     }
